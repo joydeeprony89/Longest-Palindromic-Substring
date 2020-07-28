ae46b01754aff086c9421095b6aa737792589f4d
joydeeprony89 <joydeeprony89@gmail.com>
2020-07-28 11:54:23 +0200

    Add project files.

---
+++ Longest Palindromic Substring/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Longest_Palindromic_Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine(program.LongestPalindrome("forgeeksskeegfor"));
+            Console.WriteLine("Hello World!");
+        }
+
+        public string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return "";
+            var arr = s.ToCharArray(); 
+            string result = arr[0].ToString();
+
+            int length = s.Length;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (PalindromeExist(s, i, j, arr))
+                    {
+                        string newPalindrome = s.Substring(i, j - i + 1);
+                        result = newPalindrome.Length > result.Length ? newPalindrome : result;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private bool PalindromeExist(string s, int start, int end, char[] arr)
+        {
+            for (int i = start, j = end; i < end; i++)
+            {
+                if (arr[i] == arr[j--]) continue;
+                else return false;
+            }
+
+            return true;
+        }
+    }
+}
